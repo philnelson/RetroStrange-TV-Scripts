eaed924ee7100f869ceb5d541e22f519e9547789
Phil Nelson <phil@extrafuture.com>
2024-09-01 23:00:37 -0700

    Many formatting changes to metadata script, and quality of life stuff for GIF pages such as OpenGraph shiz

--- generate_gif_gallery.py
+++ generate_gif_gallery.py
@@ -1,8 +1,9 @@
 import os
+import urllib.parse
 
 import rstv_config
 
-def generate_html(folder_path):
+def generate_media_html(folder_path):
     item_name = os.path.basename(folder_path)
     # Generate HTML content for a folder
     html_content = f"""
@@ -42,6 +43,7 @@ def generate_html(folder_path):
     </head>
     <body>
         <h1>{item_name}</h1>
+        <h2>Back to <a href="https://retrostrange.com/gifs">RetroStrange GIFs Gallery</a></h2>
         <ul>
     """
 
@@ -49,11 +51,76 @@ def generate_html(folder_path):
     gif_files = [file for file in os.listdir(folder_path) if file.lower().endswith('.gif')]
 
     for gif_file in gif_files:
-        html_content += f"            <li><a href=\"{gif_file}\"><img src=\"{gif_file}\" alt=\"{gif_file}\"></a></li>\n"
+        html_content += f"            <li><a href=\"{gif_file}.html\"><img src=\"{gif_file}\" alt=\"{gif_file}\"></a></li>\n"
 
     # Close HTML tags
     html_content += """
         </ul>
+        <p>Brought to you by <a href="https://retrostrange.com">RetroStrange</a>. Watch our 24/7 stream on <a href="https://live.retrostrange.com">RetroStrange TV</a>.</p>
+    </body>
+    </html>
+    """
+
+    return html_content
+
+def generate_individual_html(file_path, folder):
+    item_name = os.path.basename(file_path)
+    # Generate HTML content for a folder
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>RetroStrange GIFs: {item_name[:-10]}</title>
+        <meta property="og:image:secure_url" content="https://retrostrange.com/gifs/{urllib.parse.quote(folder)}/{urllib.parse.quote(file_path)}"
+            class="black-bgcolor" />
+
+        <meta property="og:image:type" content="image/gif" class="black-bgcolor" />
+
+        <meta property="og:image:width" content="300" class="black-bgcolor" />
+
+        <meta property="og:image:height" content="300" class="black-bgcolor" />
+        <style>
+            body {{
+                font-family: monospace;
+                margin: 20px 40px 20px 40px;
+            }}
+            h1 {{
+                text-align: center;
+                font-size: 3.5em;
+            }}
+            ul {{
+                list-style: none;
+                padding: 0;
+                display: grid;
+                grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+                grid-gap: 10px;
+            }}
+            li {{
+                text-align: center;
+                font-size: 2em;
+            }}
+            p {{
+                font-size: 2em;
+                line-height: 2.2em;
+            }}
+            img {{
+                max-width: 100%;
+                height: auto;
+            }}
+            
+            input[type=text] {{
+                width: 300px; 
+                height: 40px;
+                font-size: 1.2em;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>{item_name[:-10]}</h1>
+        <h2>Back to <a href="https://retrostrange.com/gifs">RetroStrange GIFs Gallery</a>, "<a href="https://retrostrange.com/gifs/{folder}/{folder}.html">{folder}</a>" section</h2>
+        <img src="{file_path}" />
+        <p>Download or share this file:</p>
+        <input type="text" value="https://retrostrange.com/gifs/{urllib.parse.quote(folder)}/{urllib.parse.quote(file_path)}" />
         <p>Brought to you by <a href="https://retrostrange.com">RetroStrange</a>. Watch our 24/7 stream on <a href="https://live.retrostrange.com">RetroStrange TV</a>.</p>
     </body>
     </html>
@@ -104,7 +171,7 @@ def generate_root_html(directory_path, folder_names):
     # List all folder names and link to their respective pages
     for folder_name in folder_names:
         folder_page_link = f"<a href=\"{folder_name}/{folder_name}.html\">{folder_name}</a>"
-        html_content += f"            <li><img src=\"{folder_name}/{folder_name}.1.gif\" />{folder_page_link}</li>\n"
+        html_content += f"            <li>{folder_page_link}</li>\n"
 
     # Close HTML tags
     html_content += """
@@ -129,7 +196,7 @@ def generate_html_pages(root_directory):
         
         # Generate HTML for each subdirectory
         subdirectory_path = os.path.join(root_directory, subdirectory)
-        html_content = generate_html(subdirectory_path)
+        html_content = generate_media_html(subdirectory_path)
         
         print(f"Generating {subdirectory}")
 
@@ -137,6 +204,16 @@ def generate_html_pages(root_directory):
         html_file_path = os.path.join(subdirectory_path, f"{subdirectory}.html")
         with open(html_file_path, "w") as html_file:
             html_file.write(html_content)
+            
+        # Get all GIFs in the folder
+        gif_files = [file for file in os.listdir(subdirectory_path) if file.lower().endswith('.gif')]
+        gif_files.sort(reverse = True)
+        
+        for gif_file in gif_files:
+            individual_html_content = generate_individual_html(gif_file, subdirectory)
+            individual_html_file_path = os.path.join(subdirectory_path, f"{gif_file}.html")
+            with open(individual_html_file_path, "w") as individual_html_file:
+                individual_html_file.write(individual_html_content)
 
     # Generate HTML for the root page
     generate_root_html(root_directory, subdirectories)
--- rstv_metadata_checkup.py
+++ rstv_metadata_checkup.py
@@ -112,7 +112,7 @@ def get_video_title(file_path):
 def create_gifs_from_video(file, start, duration, seq):
     filename = os.path.basename(file)
     path = filename[:-4]
-    print(f'{generator_config["gifs_path"]}{path}/{filename}.{seq}.gif')
+    #print(f'{generator_config["gifs_path"]}{path}/{filename}.{seq}.gif')
     result = subprocess.run(
         [
             "ffmpeg",
@@ -153,6 +153,23 @@ def create_gifs_from_video(file, start, duration, seq):
     
 tic = time.perf_counter()
 
+print("""##########################################################################################
+################################################     ###     #      #     #####    #######
+################################################  ##  ##  ######  ###  ##  ##   ##   #####
+####                                       #####      ##     ###  ###      #   #####  ####
+####                                       #####  #  ###  ######  ###     ###  ####   ####
+################################################  ##  ##     ###  ###  ##  ###      ######
+##########################################################################################
+####                                                                                  ####
+####                                                                                  ####
+##########################################################################################
+#####     #      #      ####   ####   ####  ###      ###     #############################
+####   ######  ###  ##  ####    ###     ##  ##  #### ###  ######                      ####
+######    ###  ###     ####  ##  ##   #  #  ##  ##     #     ###                      ####
+########  ###  ###  #   ##        #   ##    ##   ###  ##  ################################
+#####    ####  ###  ##      ####  #   ####  ####    ####     #############################
+##########################################################################################""")
+total_files_in_catalog = 0
 for media_type in generator_config['types']:
     
     print("------")
@@ -164,13 +181,7 @@ for media_type in generator_config['types']:
         good_files = []
         weird_files = []
         weird_file_detail = {}
-        print(" /\                 /\\")
-        print("/ \\'._   (\_/)   _.'/ \\")
-        print("|.''._'--(o.o)--'_.''.|")
-        print(" \_ / `;=/ \" \=;` \ _/")
-        print("   `\__| \___/ |__/`")
-        print("        \(_|_)/")
-        print("         \" ` \"")
+        
         print("🦇 SOARING MAJESTICALLY OVER DIRECTORY {} 🦇".format(path))
         print(" ")
         # Check for all *.mp4" filenames
@@ -180,6 +191,7 @@ for media_type in generator_config['types']:
                 if not file.startswith('.'):
                     if ".mp4" in file or ".m4v" in file:
                         if ".gif" not in file:
+                            #print(file)
                             # Check for year in filename
                             if bool(re.search(r'\(\d{4}\)', file)):
                                 good_files.append(os.path.join(r, file))
@@ -194,6 +206,8 @@ for media_type in generator_config['types']:
 
         print("")
         print("🦇 FOUND {} PROPERLY FORMATTED VIDEO FILES 🦇".format(len(good_files)))
+        total_files_to_scan = len(good_files)
+        files_scanned_so_far = 0
         for f in good_files:
             filename= os.path.basename(f)
             video_length_in_seconds = video_length_seconds(f)
@@ -205,18 +219,23 @@ for media_type in generator_config['types']:
                 data = file_to_check.read()    
                 # pipe contents of the file through
                 md5_returned = hashlib.md5(data).hexdigest()
+                
+            file_size = os.path.getsize(f)
+            file_size_mb = math.ceil((file_size / 1024)/1024)
             
-            generator_config['types'][media_type]['items'][md5_returned] = {"video_title": video_title, "path": f, "filename": filename, "duration_in_seconds": video_length_in_seconds}
+            generator_config['types'][media_type]['items'][md5_returned] = {"video_title": video_title, "path": f, "filename": filename, "size": file_size, "duration_in_seconds": video_length_in_seconds}
             
-            #print(filename, video_length_in_seconds, "sec")
+            print("{} [{}/{}] {}".format(media_type, files_scanned_so_far+1, total_files_to_scan, md5_returned)) 
+            print("\tFile: {} \n\tDuration: {}s, Size: {}mb".format(filename, video_length_in_seconds, file_size_mb))
 
+            number_of_gifs = 7
             if generator_config['create_gifs']:
-                first_gif_start = 15
-                second_gif_start = math.floor(video_length_in_seconds / 4)
-                third_gif_start = math.floor(video_length_in_seconds / 4) * 2
-                fourth_gif_start = math.floor(video_length_in_seconds / 4) * 3
-                fifth_gif_start = math.floor(video_length_in_seconds) - 180
-                sixth_gif_start = math.floor(video_length_in_seconds) - 90
+                first_gif_start = math.floor(video_length_in_seconds / 7)
+                second_gif_start = math.floor(video_length_in_seconds / 7) * 2
+                third_gif_start = math.floor(video_length_in_seconds / 7) * 3
+                fourth_gif_start = math.floor(video_length_in_seconds / 7) * 4
+                fifth_gif_start = math.floor(video_length_in_seconds / 7) * 5
+                sixth_gif_start = math.floor(video_length_in_seconds / 7) * 6
                 seventh_gif_start = math.floor(video_length_in_seconds) - 30
                 
                 if not os.path.exists("{}{}".format(generator_config["gifs_path"], filename[:-4])): 
@@ -225,11 +244,11 @@ for media_type in generator_config['types']:
                 #print("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4], filename, 1))
                       
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4], filename, 1)):
-                    print("GENERATING GIF1 at {}".format(first_gif_start))
+                    print("\tGenerating GIF1 at {}".format(first_gif_start))
                     create_gifs_from_video(f, first_gif_start, 2.5, 1)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF1 at {}".format(first_gif_start))
+                        print("\tGenerating GIF1 at {}".format(first_gif_start))
                         create_gifs_from_video(
                             f, first_gif_start, 2.5, 1)
                     #else:
@@ -237,11 +256,11 @@ for media_type in generator_config['types']:
                     #        first_gif_start))
 
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 2)):
-                    print("GENERATING GIF2 at {}".format(second_gif_start))
+                    print("\tGenerating GIF2 at {}".format(second_gif_start))
                     create_gifs_from_video(f, second_gif_start, 2.5, 2)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF2 at {}".format(second_gif_start))
+                        print("Overwriting GIF2 at {}".format(second_gif_start))
                         create_gifs_from_video(
                             f, second_gif_start, 2.5, 2)
                     #else:
@@ -249,11 +268,11 @@ for media_type in generator_config['types']:
                     #        second_gif_start))
 
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 3)):
-                    print("GENERATING GIF3 at {}".format(third_gif_start))
+                    print("\tGenerating GIF3 at {}".format(third_gif_start))
                     create_gifs_from_video(f, third_gif_start, 2.5, 3)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF3 at {}".format(third_gif_start))
+                        print("\tOverwriting GIF3 at {}".format(third_gif_start))
                         create_gifs_from_video(
                             f, third_gif_start, 2.5, 1)
                     #else:
@@ -261,11 +280,11 @@ for media_type in generator_config['types']:
                     #        third_gif_start))
 
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 4)):
-                    print("GENERATING GIF4 at {}".format(fourth_gif_start))
+                    print("\tGenerating GIF4 at {}".format(fourth_gif_start))
                     create_gifs_from_video(f, fourth_gif_start, 2.5, 4)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF4 at {}".format(fourth_gif_start))
+                        print("\tOverwriting GIF4 at {}".format(fourth_gif_start))
                         create_gifs_from_video(
                             f, fourth_gif_start, 2.5, 1)
                     #else:
@@ -273,11 +292,11 @@ for media_type in generator_config['types']:
                     #        fourth_gif_start))
                             
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 5)):
-                    print("GENERATING GIF5 at {}".format(fifth_gif_start))
+                    print("\tGenerating GIF5 at {}".format(fifth_gif_start))
                     create_gifs_from_video(f, (fifth_gif_start), 2.5, 5)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF5 at {}".format((fifth_gif_start)))
+                        print("\tOverwriting GIF5 at {}".format((fifth_gif_start)))
                         create_gifs_from_video(
                             f, (fifth_gif_start), 2.5, 1)
                     #else:
@@ -285,11 +304,11 @@ for media_type in generator_config['types']:
                         #    (fifth_gif_start)))
                 
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 6)):
-                    print("GENERATING GIF6 at {}".format(sixth_gif_start))
+                    print("Generating GIF6 at {}".format(sixth_gif_start))
                     create_gifs_from_video(f, (sixth_gif_start), 2.5, 6)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF5 at {}".format((sixth_gif_start)))
+                        print("\tOverwriting GIF5 at {}".format((sixth_gif_start)))
                         create_gifs_from_video(
                             f, (sixth_gif_start), 2.5, 1)
                     #else:
@@ -297,21 +316,23 @@ for media_type in generator_config['types']:
                         #    (sixth_gif_start)))
                 
                 if not os.path.exists("{}{}/{}.{}.gif".format(generator_config["gifs_path"],filename[:-4],filename, 7)):
-                    print("GENERATING GIF7 at {}".format(seventh_gif_start))
+                    print("\tGenerating GIF7 at {}".format(seventh_gif_start))
                     create_gifs_from_video(f, (seventh_gif_start), 2.5, 7)
                 else:
                     if generator_config['overwrite_gifs']:
-                        print("OVERWRITING GIF7 at {}".format((seventh_gif_start)))
+                        print("\tOverwriting GIF7 at frame {}".format((seventh_gif_start)))
                         create_gifs_from_video(
                             f, (seventh_gif_start), 2.5, 1)
                     #else:
                         #print("GIF6 at {} exists, skipping".format(
                         #    (seventh_gif_start)))
-
+            files_scanned_so_far +=1
+            total_files_in_catalog +=1
         print(" ")
         print("🦇 FOUND {} WEIRD FILES IN THE DIRECTORY 🦇".format(len(weird_files)))
         for f in weird_files:
             filename= os.path.basename(f)
+            print(filename)
             video_length_in_seconds = video_length_seconds(f)
             video_title = get_video_title(f)
             total_length_in_seconds += math.floor(video_length_seconds(f))
@@ -322,9 +343,10 @@ for media_type in generator_config['types']:
                 # pipe contents of the file through
                 md5_returned = hashlib.md5(data).hexdigest()
             
-            generator_config['types'][media_type]['weird_file_detail'][md5_returned] = {"video_title": video_title, "path": f, "filename": filename, "duration_in_seconds": video_length_in_seconds}
-            
-            print(filename, video_length_in_seconds, "sec")
+            file_size = os.path.getsize(f)
+            file_size_mb = math.ceil((file_size / 1024)/1024)
+
+            generator_config['types'][media_type]['weird_file_detail'][md5_returned] = {"video_title": video_title, "path": f, "filename": filename, "size": file_size, "duration_in_seconds": video_length_in_seconds}
 
         print(" ")
         print("🦇 STATS FOR THE {} PROPERLY FORMATTED FILES 🦇".format(len(good_files)))
@@ -345,37 +367,38 @@ print("Updated {} media types: {}".format(
 all_media_total_length_in_seconds = 0
 for media_type in updated_types:
     all_media_total_length_in_seconds += updated_types[media_type]['total_length_in_seconds']
-    print(" {}: {} good files, {} weird files".format(media_type, len(
+    print(" {}: {} clean files, {} files with issues".format(media_type, len(
         updated_types[media_type]['good_files']), len(updated_types[media_type]['weird_files'])))
 
     print("    Total duration: {}".format(get_fancy_duration(updated_types[media_type]['total_length_in_seconds'])))
 
-with open('rstv media list good files.csv', 'w', newline='') as csvfile:
+with open('{}/rstv media list good files.csv'.format(rstv_config.catalog_output_path), 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Title','Media Type','Filename', 'Duration In Seconds','Full Path','md5','Issues'])
+    spamwriter.writerow(['Title','Media Type','Filename', 'Size (In Bytes)', 'Duration In Seconds','Full Path','md5','Issues'])
     for media_type in updated_types:
         for item in generator_config['types'][media_type]['items']:
-            spamwriter.writerow([generator_config['types'][media_type]['items'][item]['video_title'], media_type, generator_config['types'][media_type]['items'][item]['filename'], generator_config['types'][media_type]['items'][item]['duration_in_seconds'],generator_config['types'][media_type]['items'][item]['path'], item])
+            spamwriter.writerow([generator_config['types'][media_type]['items'][item]['video_title'], media_type, generator_config['types'][media_type]['items'][item]['filename'], generator_config['types'][media_type]['items'][item]['size'], generator_config['types'][media_type]['items'][item]['duration_in_seconds'],generator_config['types'][media_type]['items'][item]['path'], item])
             
-with open('rstv media weird files only.csv', 'w', newline='') as csvfile:
+with open('{}/rstv media weird files only.csv'.format(rstv_config.catalog_output_path), 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Title','Media Type','Filename', 'Duration In Seconds','Full Path','md5','Issues'])
+    spamwriter.writerow(['Title','Media Type','Filename', 'Size (In Bytes)', 'Duration In Seconds','Full Path','md5','Issues'])
     for media_type in updated_types:
         for item in generator_config['types'][media_type]['weird_file_detail']:
-            spamwriter.writerow([generator_config['types'][media_type]['weird_file_detail'][item]['video_title'], media_type, generator_config['types'][media_type]['weird_file_detail'][item]['filename'], generator_config['types'][media_type]['weird_file_detail'][item]['duration_in_seconds'],generator_config['types'][media_type]['weird_file_detail'][item]['path'], item, "Bad Metadata"])
+            spamwriter.writerow([generator_config['types'][media_type]['weird_file_detail'][item]['video_title'], media_type, generator_config['types'][media_type]['weird_file_detail'][item]['filename'], generator_config['types'][media_type]['weird_file_detail'][item]['size'], generator_config['types'][media_type]['weird_file_detail'][item]['duration_in_seconds'],generator_config['types'][media_type]['weird_file_detail'][item]['path'], item, "Bad Metadata"])
             
-with open('rstv media list combined.csv', 'w', newline='') as csvfile:
+with open('{}/rstv media list combined.csv'.format(rstv_config.catalog_output_path), 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Title','Media Type','Filename', 'Duration In Seconds','Full Path','md5','Issues'])
+    spamwriter.writerow(['Title','Media Type','Filename', 'Size (In Bytes)', 'Duration In Seconds','Full Path','md5','Issues'])
     for media_type in updated_types:
         for item in generator_config['types'][media_type]['items']:
-            spamwriter.writerow([generator_config['types'][media_type]['items'][item]['video_title'], media_type, generator_config['types'][media_type]['items'][item]['filename'], generator_config['types'][media_type]['items'][item]['duration_in_seconds'],generator_config['types'][media_type]['items'][item]['path'], item])
+            spamwriter.writerow([generator_config['types'][media_type]['items'][item]['video_title'], media_type, generator_config['types'][media_type]['items'][item]['filename'],generator_config['types'][media_type]['items'][item]['size'], generator_config['types'][media_type]['items'][item]['duration_in_seconds'],generator_config['types'][media_type]['items'][item]['path'], item])
         for item in generator_config['types'][media_type]['weird_file_detail']:
-            spamwriter.writerow([generator_config['types'][media_type]['weird_file_detail'][item]['video_title'], media_type, generator_config['types'][media_type]['weird_file_detail'][item]['filename'], generator_config['types'][media_type]['weird_file_detail'][item]['duration_in_seconds'],generator_config['types'][media_type]['weird_file_detail'][item]['path'], item, "Bad Metadata"])
+            spamwriter.writerow([generator_config['types'][media_type]['weird_file_detail'][item]['video_title'], media_type, generator_config['types'][media_type]['weird_file_detail'][item]['filename'], generator_config['types'][media_type]['weird_file_detail'][item]['size'], generator_config['types'][media_type]['weird_file_detail'][item]['duration_in_seconds'],generator_config['types'][media_type]['weird_file_detail'][item]['path'], item, "Bad Metadata"])
 
 toc = time.perf_counter()
-print("Duration of all media: {}".format(get_fancy_duration(all_media_total_length_in_seconds)))
-print(f"Took {toc - tic:0.4f} seconds. All done.")
+print("Total files: {}".format(total_files_in_catalog))
+print("Duration of all scanned media: {}".format(get_fancy_duration(all_media_total_length_in_seconds)))
+print(f"Took {toc - tic:0.4f} seconds to scan. All done.")
